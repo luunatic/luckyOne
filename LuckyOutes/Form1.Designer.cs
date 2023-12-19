ce8279c1aebaaaf769085c7faf326df5fbab81de
Rosaline <61531954+luunatic@users.noreply.github.com>
2023-12-19 16:17:13 +0800

    Add files via upload

---
+++ LuckyOutes/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace LuckyOutes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(49, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "OUTES抽奖小程序";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("隶书", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(443, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(386, 112);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "幸运儿";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(479, 524);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(313, 68);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "抽取";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(1310, 650);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Outes幸运奖";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ LuckyOutes/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace LuckyOutes
+{
+    // 定义一个比较器
+   
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Set the form properties
+            this.StartPosition = FormStartPosition.CenterScreen;
+            
+        }
+        ThreadStart ts;
+        Thread t;
+        delegate void StringArgsDelegate(string s);
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ts = new ThreadStart(UpdateUI);
+            t = new Thread(ts);
+            t.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (t.IsAlive)
+            {
+                t.Abort();
+                button1.Text = "重新抽取";
+            }
+            else
+            {
+                t = new Thread(ts);
+                t.Start();
+                button1.Text = "抽取";
+            }
+
+           
+        }
+        private void UpdateLabel(string s)
+        {
+            label2.Text = s;
+           
+        }
+        private void UpdateUI()
+        {
+            string path = "抽奖名单.txt";
+            FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(file);
+            List<string> names = sr.ReadToEnd().Split(new char[] { '\n' }).ToList();
+            while (true)
+            {
+                names.Sort(new RandomComparer());
+                foreach (string name in names)
+                {
+                    this.BeginInvoke(new StringArgsDelegate(UpdateLabel), name);
+                    Thread.Sleep(20);
+                }
+            }
+            
+        }
+    }
+    class RandomComparer : IComparer<string>
+    {
+        Random random = new Random();
+        public int Compare(string x, string y)
+        {
+            // 返回-1或1，让Sort()方法随机比较元素，从而实现随机排序
+            return random.Next(0, 2) * 2 - 1;
+        }
+    }
+}
